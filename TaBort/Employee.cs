a74c6922f6e6ab0432393f3b82a84943a7b27559
Fadi Mashan <fadi.mashan@gmail.com>
2021-01-19 16:22:08 +0100

    Done with two calsses: Program class (the main one) Employee class with two attribut (Name and Salary)

---
+++ TaBort/Employee.cs
@@ -0,0 +1,14 @@
+﻿namespace TaBort
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public int Salary { get; set; }
+
+        public Employee(string name, int salary)
+        {
+            Name = name;
+            Salary = salary; 
+        }
+    }
+}
--- TaBort/Program.cs
+++ TaBort/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace TaBort
 {
@@ -6,7 +7,43 @@ namespace TaBort
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Employee> employees = new List<Employee>();
+
+            while (true)
+            {
+                Console.WriteLine("Enter: \n(1) for adding new employee. \n(2) to see the registered employees");
+            var number = Console.ReadLine();
+
+           
+                switch (int.Parse(number))
+                {
+                    case 1:
+                        Console.WriteLine("write employee name");
+                        var eName = Console.ReadLine();
+                        Console.WriteLine("write employee name");
+                        var eSalary = Console.ReadLine();
+
+                        employees.Add(new Employee(eName, int.Parse(eSalary)));
+
+                        break;
+
+                    case 2:
+
+                       
+                        for (int i = 0; i < employees.Count; i++)
+                        {
+                            Console.WriteLine($"Employee{i+1} name is: {employees[i].Name}, Salary is:{employees[i].Salary} \n");
+
+                        }
+
+                        break;
+
+                }
+            }
+
+            
         }
+
+     
     }
 }
