5024a03628b5792d5a3b4c1b2ab228322bddbeac
Fadi Mashan <fadi.mashan@gmail.com>
2021-01-20 10:38:24 +0100

    -Added some condition for entring a number for the salary. - Fix the output design - Added therd option to exit.

--- TaBort/Program.cs
+++ TaBort/Program.cs
@@ -7,43 +7,87 @@ namespace TaBort
     {
         static void Main(string[] args)
         {
+            bool loopCondition = true;
             List<Employee> employees = new List<Employee>();
 
-            while (true)
+            while (loopCondition)
             {
-                Console.WriteLine("Enter: \n(1) for adding new employee. \n(2) to see the registered employees");
-            var number = Console.ReadLine();
+                Console.WriteLine(" ______________________________________");
+                Console.WriteLine("Enter: \n(1) for adding new employee. \n(2) to see the registered employees. \n(3) Exit.");
 
-           
-                switch (int.Parse(number))
+                var input = Console.ReadLine();
+                int number;
+
+                if (int.TryParse(input, out number))
                 {
-                    case 1:
-                        Console.WriteLine("write employee name");
-                        var eName = Console.ReadLine();
-                        Console.WriteLine("write employee name");
-                        var eSalary = Console.ReadLine();
+                    number = int.Parse(input);
 
-                        employees.Add(new Employee(eName, int.Parse(eSalary)));
-
-                        break;
-
-                    case 2:
-
-                       
-                        for (int i = 0; i < employees.Count; i++)
+                    if (number == 1 || number == 2 || number == 3)
+                    {
+                        switch (number)
                         {
-                            Console.WriteLine($"Employee{i+1} name is: {employees[i].Name}, Salary is:{employees[i].Salary} \n");
+                            case 1:
+                                Console.WriteLine("write employee name");
+                                var eName = Console.ReadLine();
+                                bool isSalary = true;
+                                Console.WriteLine("write employee salary");
+                                while (isSalary)
+                                {
+                                 
+                                    var eSalary = Console.ReadLine();
+                                    int salary;
+
+                                    if (int.TryParse(eSalary, out salary))
+                                    {
+                                        employees.Add(new Employee(eName, int.Parse(eSalary)));
+                                        isSalary = false;
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("Salary should be numpers, Please enter the salary!");
+                                        isSalary = true;
+
+                                    }
+                                }
+                                break;
+
+                            case 2:
+
+                                if (employees.Count != 0)
+                                {
+                                    for (int i = 0; i < employees.Count; i++)
+                                    {
+                                        Console.WriteLine(" ______________________________________");
+                                        Console.WriteLine($"Employee{i + 1} name is: {employees[i].Name}, Salary is:{employees[i].Salary}");
+                                    }
+                                }
+                                else
+                                {
+                                    Console.WriteLine("There is no employees registerd!");
+                                }
+
+                                break;
+
+                            case 3:
+                                loopCondition = false;
+                                Console.WriteLine("Thanks, Bye!");
+                                break;
 
                         }
-
-                        break;
-
+                    }
+                    else
+                    {
+                        Console.WriteLine("Enter option number (1 or 2)");
+                    }
                 }
-            }
+                else
+                {
+                    Console.WriteLine("Enter option number (1 or 2)");
+                }
 
-            
+
+            }
         }
 
-     
     }
 }
